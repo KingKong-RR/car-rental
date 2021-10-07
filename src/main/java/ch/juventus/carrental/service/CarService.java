4fef8f27cd5c8f90cb9b58f82516ad7473c7b715
Pietro Stäheli <pietro.staeheli@juventus.schule>
2021-10-07 21:27:45 +0200

    use interface

--- src/main/java/ch/juventus/carrental/service/CarService.java
+++ src/main/java/ch/juventus/carrental/service/CarService.java
@@ -1,18 +1,19 @@
 package ch.juventus.carrental.service;
 
+import ch.juventus.carrental.persistance.Database;
 import ch.juventus.carrental.persistance.FileDatabase;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarService {
-    private final FileDatabase fileDatabase;
+    private final Database database;
 
     public CarService(FileDatabase fileDatabase) {
-        this.fileDatabase = fileDatabase;
+        this.database = fileDatabase;
     }
 
     public String getHelloWorldGreeting() {
-        return fileDatabase.loadHelloWorldGreeting();
+        return database.loadHelloWorldGreeting();
     }
 
 }
