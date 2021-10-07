3ab77d763eb814b84985b8581b4de815641a2564
Pietro Stäheli <pietro.staeheli@juventus.schule>
2021-10-07 21:22:10 +0200

    add FileDatabase class

---
+++ src/main/java/ch/juventus/carrental/persistance/FileDatabase.java
@@ -0,0 +1,13 @@
+package ch.juventus.carrental.persistance;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FileDatabase {
+
+    public String loadHelloWorldGreeting() {
+        return "Hello, world!";
+    }
+
+
+}
--- src/main/java/ch/juventus/carrental/service/CarService.java
+++ src/main/java/ch/juventus/carrental/service/CarService.java
@@ -1,12 +1,18 @@
 package ch.juventus.carrental.service;
 
+import ch.juventus.carrental.persistance.FileDatabase;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarService {
+    private final FileDatabase fileDatabase;
+
+    public CarService(FileDatabase fileDatabase) {
+        this.fileDatabase = fileDatabase;
+    }
 
     public String getHelloWorldGreeting() {
-        return "Hello, world!";
+        return fileDatabase.loadHelloWorldGreeting();
     }
 
 }
