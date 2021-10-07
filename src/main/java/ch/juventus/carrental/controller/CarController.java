a0af06d2869ebef428d4b0a7666c5485974aa2fa
Pietro Stäheli <pietro.staeheli@juventus.schule>
2021-10-07 20:44:30 +0200

    hello, world!

--- src/main/java/ch/juventus/carrental/controller/CarController.java
+++ src/main/java/ch/juventus/carrental/controller/CarController.java
@@ -1,4 +1,15 @@
 package ch.juventus.carrental.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+    @GetMapping("/helloWorld")
+    public ResponseEntity<String> helloWorld() {
+        String response = "Hello world!";
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
