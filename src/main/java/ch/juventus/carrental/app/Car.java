e7d5697494f3d6b84f71e32064f34ea0b9ec9b50
Pietro Stäheli <pietro.staeheli@juventus.schule>
2021-12-28 18:40:54 +0100

    some things work now but toString override missing

--- src/main/java/ch/juventus/carrental/app/Car.java
+++ src/main/java/ch/juventus/carrental/app/Car.java
@@ -9,6 +9,75 @@ public class Car {
     private int pricePerDay;
     private Boolean airCondition;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGearShift() {
+        return gearShift;
+    }
+
+    public void setGearShift(String gearShift) {
+        this.gearShift = gearShift;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(int pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Boolean getAirCondition() {
+        return airCondition;
+    }
+
+    public void setAirCondition(Boolean airCondition) {
+        this.airCondition = airCondition;
+    }
+
+    public Car(int id, String name, String type, String gearShift, int seats, int pricePerDay, Boolean airCondition) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.gearShift = gearShift;
+        this.seats = seats;
+        this.pricePerDay = pricePerDay;
+        this.airCondition = airCondition;
+    }
+
+    public Car() {
+    }
+
     enum Type {
         CABRIO,
         LIMOUSINE,
--- src/main/java/ch/juventus/carrental/controller/CarController.java
+++ src/main/java/ch/juventus/carrental/controller/CarController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class CarController {
 
@@ -36,6 +39,25 @@ public class CarController {
     @GetMapping("/api/v1/cars/{id}")
     public ResponseEntity<String> sef(@PathVariable Long id, @RequestBody Car updatedCar) {
         return null;
+    }
+
+    @GetMapping(path="/api/v1/cars", produces = "application/json")
+    public ResponseEntity<String> getCars() {
+        //return (List<Car>) new ResponseEntity<List<Car>>(carService.getCars(), HttpStatus.OK);
+        //logger.info("Hit API endpoint, showing " + carService.getCars());
+        //return carService.getCars();
+        ArrayList<Car> carList = new ArrayList<>();
+        Car car1 = new Car(1, "Mercedes E", "LIMOUSINE", "MANUAL", 4, 100, false);
+        Car car2 = new Car(2, "BMW 5", "LIMOUSINE", "AUTOMATIC", 4, 120, true);
+        Car car3 = new Car(3, "Toyota Prius", "COUPE", "AUTOMATIC", 2, 80, true);
+        //carList.add(1, car1);
+        //carList.add(2, car2);
+        //carList.add(3, car3);
+        //carList.add(1, car1);
+        carList.add(new Car(1, "Fiat", "COUPE", "MANUAL", 4, 100, false));
+        //String response = "im here";
+
+        return new ResponseEntity<>(carList.toString(), HttpStatus.OK);
     }
 
     // http://localhost:8080/api/va/cars?filter={airCondition: false, type: SUV, etc}
